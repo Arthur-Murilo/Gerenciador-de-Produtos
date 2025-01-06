101b457fe5f4070c94339df043a39e6d369ea143
Arthur-Murilo <arthur.msc1811@hotmail.com>
2025-01-06 20:54:59 -0300

    Melhorias realizadas como adição de opção de exclusão de produtos, resolução de problemas e perfomance

--- Gerenciador_de_Produtos.py
+++ Gerenciador_de_Produtos.py
@@ -1,5 +1,6 @@
 import os
-
+import json as js
+import time 
 
 def limpar():
     return os.system("cls" if os.name == "nt" else "clear")
@@ -10,7 +11,6 @@ def cadastrar(produtos):
         try:
             print("=== Sistema de Cadastro de Produtos ===\n")
             nome = input("Informe o nome do produto: ")
-            nome = nome.lower()
             nome = nome.capitalize()
             preco = float(input("Informe o preço do produto: "))
             quantidade = int(input("Informe a quantidade em estoque: "))
@@ -26,21 +26,21 @@ def cadastrar(produtos):
 
 
 def listar(produtos):
-    print("""+------------+-------+------------+
-| Nome       | Preço | Quantidade |
-+------------+-------+------------+""")
+    col_width = max(len(prod) for prod in produtos["Nome"]) + 2
+    col2_width = max(len(str(prod)) for prod in produtos["Preco"]) + 2
+    col3_width = max(len(str(prod)) for prod in produtos["Quantidade"]) + 7
+    print(f'+{"-" * col_width}+{"-" * col2_width}----+{"-" * col3_width}---+')
+    print(f'| {"Nome".ljust(col_width)} | {'Preço'.ljust(col2_width)} | {'Quantidade'.ljust(col3_width)} |')
+    print(f'+{"-" * col_width}+{"-" * col2_width}----+{"-" * col3_width}---+')
 
     for i in range(len(produtos["Nome"])):
-        print(
-            f'| {produtos["Nome"][i]:<10} | {produtos["Preco"][i]:<5.2f} | {"":<4} {produtos["Quantidade"][i]:<5} |'
-        )
-    print("+------------+-------+------------+")
+        print(f'| {produtos["Nome"][i].ljust(col_width)} | {str(produtos["Preco"][i]).ljust(col2_width)} | {str(produtos["Quantidade"][i]).ljust(col3_width)} |')
+        print(f'+{"-" * col_width}+{"-" * col2_width}----+{"-" * col3_width}---+')
 
 
 def buscar(produtos):
     while True:
         nome = input("Qual nome do produto que você deseja achar?: ")
-        nome = nome.lower()
         nome = nome.capitalize()
         try:
             limpar()
@@ -66,82 +66,65 @@ def atualizar(produtos):
                 "== Caso não queira mudar todos apenas aperte Enter no que não quiser mudar"
             )
             nome = input("Informe o novo nome do produto: ")
-            nome = nome.lower()
             nome = nome.capitalize()
             preco = input("Informe o novo preço do produto: ")
             quantidade = input("Informe a nova quantidade em estoque: ")
 
-            if nome == "":
-                if preco == "":
-                    produtos["Quantidade"][produtos["Nome"].index(antigo)] = int(
-                        quantidade
-                    )
-                    break
-                elif quantidade == "":
-                    produtos["Preco"][produtos["Nome"].index(antigo)] = float(preco)
-                    break
-                elif preco == "" and quantidade == "":
-                    break
-                else:
-                    produtos["Preco"][produtos["Nome"].index(antigo)] = float(preco)
-                    produtos["Quantidade"][produtos["Nome"].index(antigo)] = int(
-                        quantidade
-                    )
-                    break
-            elif preco == "":
-                if quantidade == "":
-                    produtos["Nome"][produtos["Nome"].index(antigo)] = nome
-                    break
-                elif nome == "":
-                    produtos["Quantidade"][produtos["Nome"].index(antigo)] = int(
-                        quantidade
-                    )
-                    break
-                elif quantidade == "" and nome == "":
-                    break
-                else:
-                    produtos["Quantidade"][produtos["Nome"].index(antigo)] = int(
-                        quantidade
-                    )
-                    produtos["Nome"][produtos["Nome"].index(antigo)] = nome
-                    break
-            elif quantidade == "":
-                if preco == "":
-                    produtos["Nome"][produtos["Nome"].index(antigo)] = nome
-                    break
-                elif nome == "":
-                    produtos["Preco"][produtos["Nome"].index(antigo)] = float(preco)
-                elif preco == "" and nome == "":
-                    break
-                else:
-                    produtos["Preco"][produtos["Nome"].index(antigo)] = float(preco)
-                    produtos["Nome"][produtos["Nome"].index(antigo)] = nome
-                    break
-            else:
-                produtos["Preco"][produtos["Nome"].index(antigo)] = float(preco)
-                produtos["Quantidade"][produtos["Nome"].index(antigo)] = int(quantidade)
+            if nome:
                 produtos["Nome"][produtos["Nome"].index(antigo)] = nome
-                break
+            if preco:
+                produtos["Preco"][produtos["Nome"].index(antigo)] = float(preco)
+            if quantidade:
+                produtos["Quantidade"][produtos["Nome"].index(antigo)] = int(quantidade)
+            break
         except ValueError:
             limpar()
             print(
                 "Erro!, Verifique o nome do produto antigo, nome novo, preco novo ou nova quantidade: \n"
             )
+def excluir(produtos):
+        while True:
+            nome = input("Qual nome do produto que você deseja excluir?: ")
+            nome = nome.capitalize()
+            indice = produtos["Nome"].index(nome)
+            try:
+                limpar()
+                produtos["Nome"].remove(produtos['Nome'][indice])
+                produtos["Preco"].remove(produtos['Preco'][indice])
+                produtos["Quantidade"].remove(produtos['Quantidade'][indice])
+                print("Produto Excluido com sucesso:")
+                print("Aperte Enter para sair")
+                input()
+                break
+            except ValueError:
+                limpar()
+                print("Erro!, Nome não encontado, verificar o Nome do produto. \n")
 
 
 if __name__ == "__main__":
-    produtos = {"Nome": [], "Preco": [], "Quantidade": []}
+    try:
+        with open('produtos,json','r') as arquivo:
+            produtos = js.load(arquivo)
+    except FileNotFoundError:
+        print("Arquivo não encontrado. Criando um novo dicionário.\nDigite qualquer tecla para prosseguir")
+        produtos = {"Nome": [], "Preco": [], "Quantidade": []}
+        input()
+        limpar()
     while True:
         try:
+            limpar()
             option = int(
                 input(
-                    "=== Sistema de Gerenciamento de Produtos ===\n1. Cadastrar produto\n2. Listar produtos\n3. Buscar produto\n4. Atualizar estoque\n5. Sair\nEscolha uma opção: "
+                    "=== Sistema de Gerenciamento de Produtos ===\n1. Cadastrar produto\n2. Listar produtos\n3. Buscar produto\n4. Atualizar estoque\n5. Excluir produto\n6. Sair\nEscolha uma opção: "
                 )
             )
 
             if option == 1:
                 limpar()
                 cadastrar(produtos)
+                limpar()
+                print("Produto Cadastrado com sucesso")
+                time.sleep(2)
                 limpar()
             elif option == 2:
                 limpar()
@@ -157,7 +140,21 @@ if __name__ == "__main__":
             elif option == 4:
                 limpar()
                 atualizar(produtos)
+                limpar()
+                print("Produto Atualizado com sucesso")
+                time.sleep(2)
             elif option == 5:
+                limpar()
+                excluir(produtos)
+            elif option == 6:
+                limpar()
+                with open('produtos,json','w') as arquivo:
+                    js.dump(produtos, arquivo, indent=4)
+                print("Dados foram Salvos com Sucesso")
+                print("Saindo em...")
+                for i in range(3,0,-1):
+                    time.sleep(1)
+                    print(i) 
                 limpar()
                 print("Obrigado, Até mais.")
                 break
@@ -167,3 +164,4 @@ if __name__ == "__main__":
         except ValueError:
             limpar()
             print("Erro!, valor não relacionado\n")
+    
